edaf8f2828dd4aaeeb874c4fdea020650952d98b
Farhutdinova_RN <Farhutdinova_RN@tatneft.ru>
2024-11-11 16:12:19 +0300

    final_commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
+import TodoForm from './ToDoForm';
+import TodoList from './TodoList';
+import useTodoState from './useTodoState';
 import './styles.css';
 
 const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
+
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+            addTodo(trimmedText);
+          }
+        }}
+      />
+
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
